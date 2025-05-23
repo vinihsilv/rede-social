79a39d60862993d924d6d3f500a895138d4b71b9
Pedrohrlt <120110403+Pedrohrlt@users.noreply.github.com>
2025-05-23 19:00:09 -0300

    Delete API directory

--- API/Models/MessageModel.cs
+++
@@ -1,8 +0,0 @@
-public class Message
-{
-    public int Id { get; set; }
-    public int SenderId { get; set; }
-    public int ReceiverId { get; set; }
-    public string MessageText { get; set; }
-    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-}
--- API/README.md
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-# Meu Projeto
-=======
-# rede-social
-Projeto de Sistemas distribuídos  CC7261 - SISTEMAS DISTRIBUIDOS
->>>>>>> 0f1af68c8eac71f2d3553ce5fd36a5b566b9047a
